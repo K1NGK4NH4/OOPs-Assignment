1f26d75578770a0b61f344cfa1705a80065b31e6
Satyajit Biswal <biswalsatyajit787@gmail.com>
2025-09-26 23:59:12 +0530

    add search functionalty of vector

--- 1.cpp
+++ 1.cpp
@@ -4,39 +4,57 @@ using namespace std;
 template<typename T>
 class Vector{
 private:
-    T arr[MAX_SIZE];
-    int ind = -1;
-public:
-    void push_back(T x){
-        if(ind >= MAX_SIZE-1){
-            cout<<"Overflow"<<endl;
-            return;
+     T* arr;      
+    int capacity; 
+    int ind;      
+    //resizing new array when old is full
+    void resize() {
+        capacity = capacity * 2;
+        T* newArr = new T[capacity];
+        for (int i = 0; i <= ind; i++){
+            newArr[i] = arr[i];
         }
+        delete[] arr;
+        arr = newArr;
+    }
+public:
+     Vector(int cap = 2) {  
+        capacity = cap;
+        arr = new T[capacity];
+        ind = -1;
+    }
+    //Pushing an data back to vector
+    void push_back(T x){
         for(int i = 0; i <= ind; i++){
             if(arr[i] == x) return; // Already exists, do nothing
         }   
-        arr[++ind] = x;
+        if (ind + 1 == capacity)
+            resize();
+        ind++;
+        arr[ind] = x;
     }
-    void pop(T x){
-        if(empty()) {
-            cout<<"Empty list!!!"<<endl;
+     void pop(T x) {
+        if (ind == -1) {
+            cout << "Empty list!" << endl;
             return;
         }
+
         bool found = false;
-        for(int i=0;i<=ind;i++){
-            if(arr[i] == x){
+        for (int i = 0; i <= ind; i++) {
+            if (arr[i] == x) {
                 found = true;
-                 for(int j = i; j < ind; j++){
-                        arr[j] = arr[j + 1];
-                    }
+                for (int j = i; j < ind; j++){
+                    arr[j] = arr[j + 1];
+                }
                 ind--;
                 break;
             }
         }
-        if(!found) {
-            cout<<"No such Element exist"<<endl;
-        }
+
+        if (!found)
+            cout << "Element not found!" << endl;
     }
+
     bool empty(){
         return ind == -1;
     }
@@ -53,7 +71,14 @@ public:
         }
         cout<<endl;
     }
-};
+    std::optional<T> search(T x) {
+        for (int i = 0; i <= ind; i++) {
+            if (arr[i] == x)
+                return arr[i]; 
+        }
+        return std::nullopt; 
+    }
+ };
 int main(){
    Vector<int> v;
    v.push_back(1);
